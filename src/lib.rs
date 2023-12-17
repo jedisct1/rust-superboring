0c941f5cab944b996b3cf69f310f0c3deed99e5f
Frank Denis <github@pureftpd.org>
2023-12-17 17:24:21 +0100

    Add a really-boring feature

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,10 @@ mod implementation;
 
 #[cfg(not(feature = "enabled"))]
 mod implementation {
+    #[cfg(not(feature = "boring"))]
     pub const SUPERBORING_HAS_BEEN_DISABLED: bool = true;
+    #[cfg(feature = "boring")]
+    pub use boring::*;
 }
 
 pub use implementation::*;
